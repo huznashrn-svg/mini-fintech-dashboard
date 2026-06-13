dd1cc5b944781a6f1606962509cc0712850f5e45
Shirin K <hznshrn@gmail.com>
2026-06-13 15:42:23 +0530

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -190,7 +190,8 @@ def add_transaction():
 
     return render_template('add_transaction.html')
 
+import os
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
